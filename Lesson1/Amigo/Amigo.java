8a2fd7a9cc5023f3122d098f3599d65c8eec5a49
lroncevic <roncevicluka@outlook.com>
2023-03-08 22:05:13 +0100

    Added Lesson 1

---
+++ Lesson1/Amigo/Amigo.java
@@ -0,0 +1,8 @@
+package Amigo;
+
+public class Amigo {
+  public static void main (String[] args){
+    
+    System.out.println("Hello from Amigo :)");
+  }
+}
---
+++ Lesson1/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+import Amigo.Amigo;
+
+public class HelloWorld {
+  public static void main (String[] args){
+
+    Amigo amigo = new Amigo();
+
+    System.out.println("HelloWorld");
+    System.out.println("Hello from Amigo :)");
+  }
+}
